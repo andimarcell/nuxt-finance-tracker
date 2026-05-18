abd49d72fa947c54e30b9d4456dbb84663609f50
Andi <andimarsituru0064@gmail.com>
2026-05-18 10:41:25 +0800

    01.Persiapan Layout Untuk Project Finance Tracker
    Feat(layout): inisialisasi struktur dasar dan konfigurasi styling utama
    
    Membuat app/layouts/default.vue sebagai wrapper global dengan standarisasi lebar container (max-w-4xl) dan penempatan slot utama.
    
    Mengintegrasikan Google Fonts (Inter & Red Rose) menggunakan composable useHead untuk optimasi preconnect dan rendering teks.
    
    Mengonfigurasi Tailwind CSS v4 di assets/main.css dengan custom theme font-sans dan base layer untuk dukungan light/dark mode.
    
    Memperbarui app.vue untuk mengaktifkan sistem layouting (NuxtLayout & NuxtPage) serta penyesuaian nuxt.config.ts.

---
+++ app/layouts/default.vue
@@ -0,0 +1,26 @@
+<script setup>
+useHead({
+    link: [
+        {
+            rel: 'preconnect',
+            href: 'https://fonts.googleapis.com'
+        },
+        {
+            rel: 'preconnect',
+            href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Red+Rose:wght@300..700&display=swap',
+        }
+    ]
+    
+})
+</script>
+
+<template>
+    <div class="container mx-auto max-w-4xl">
+        <main class="my-10">
+            <slot />
+        </main>
+    </div>
+</template>
+
+<style>
+</style>
