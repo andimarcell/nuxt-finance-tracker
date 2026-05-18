f62ce7b8b7fce6bc4bbfb34ac995c75d5d469016
Andi <andimarsituru0064@gmail.com>
2026-05-19 04:24:01 +0800

    11.Menghubungkan Nuxt Dengan Supabase
    Chore(supabase): setup awal integrasi modul Supabase ke dalam project
    
    Menambahkan kredensial NUXT_PUBLIC_SUPABASE_URL dan NUXT_PUBLIC_SUPABASE_KEY menggunakan format publishable key terbaru pada file .env.
    
    Mendaftarkan modul @nuxtjs/supabase pada nuxt.config.ts.
    
    Menambahkan konfigurasi supabase: { redirect: false } agar halaman tidak diblokir saat pengembangan karena fitur autentikasi belum aktif.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui', '@nuxtjs/supabase'],
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ["~/assets/main.css"],
+  supabase: {
+    redirect: false,
+  }
 });
